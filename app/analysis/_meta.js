ca7c11c419540e99ef1785529253d90810489055
GitHubJIYEON <jiyeon2954@gmail.com>
2026-01-30 13:02:56 +0900

    빈도분석, 기술통계분석, 교차분석, 독립T검정분석

--- app/analysis/_meta.js
+++ app/analysis/_meta.js
@@ -1,9 +1,10 @@
 export default {
     whatAnalysis: "와이즈온 분석이란?",
     flowAnalysis: "분석 전체 흐름 한눈에 보기",
+    create: "분석 프로젝트 만들기",
     basic: "기본분석",
-    advancedUnivariate: "고급분석-단변량분석",
-    advancedGroupComparison: "고급분석-집단비교분석",
-    advancedScaling: "고급분석-척도화분석",
-    advancedRegression: "고급분석-회귀분석",
+    advancedUnivariate: "단변량분석",
+    advancedGroupComparison: "집단비교분석",
+    advancedScaling: "척도화분석",
+    advancedRegression: "회귀분석",
 };
