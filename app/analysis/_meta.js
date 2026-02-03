3c7298e839b6069567d2d874ca184680ca8c44a8
GitHubJIYEON <jiyeon2954@gmail.com>
2026-02-03 15:41:52 +0900

    분석, 설문 내용, 이미지 추가함

--- app/analysis/_meta.js
+++ app/analysis/_meta.js
@@ -1,6 +1,6 @@
 export default {
     whatAnalysis: "와이즈온 분석이란?",
-    flowAnalysis: "분석 전체 흐름 한눈에 보기",
+    flowAnalysis: "분석 흐름 한눈에 보기",
     create: "분석 프로젝트 만들기",
     list: "분석 프로젝트 목록 관리",
     basic: "기본분석",
